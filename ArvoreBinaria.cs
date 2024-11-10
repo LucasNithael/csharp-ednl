124ea8c5902ed3023e3c7f4530c4103f169c2d2e
Lucas Nithael <94084548+LucasNithael@users.noreply.github.com>
2024-11-10 01:28:24 +0000

    método de remoção

--- ArvoreBinaria.cs
+++ ArvoreBinaria.cs
@@ -31,7 +31,6 @@ class ArvoreBinaria{
                 }
         }
     }
-
         public No? Buscar(int valor){
             var atual = Raiz;
             while(atual != null){
@@ -45,6 +44,114 @@ class ArvoreBinaria{
                 }
             }
             return null;
+        }
+        public void Remover(int valor){
+            No? atual = Raiz;
+            No? pai = Raiz;
+            bool e_esquerda = true;
+
+            while (atual != null && atual.Valor != valor){
+                pai = atual;
+                if(valor < atual.Valor){
+                    e_esquerda = true;
+                    atual = atual.Esquerda;
+                }else{
+                    e_esquerda = false;
+                    atual = atual.Direita;
+                }
+                if(atual == null){
+                    return;
+                }
+            }
+
+            // Caso 1: Nó folha
+            if(atual != null && atual.Esquerda == null && atual.Direita == null){
+                if(atual == Raiz){
+                    Raiz = null;
+                }else if(e_esquerda){
+                    Ligacoes.Remove($"{pai.Valor} -> {atual.Valor}");
+                    pai.Esquerda = null;
+                }else{
+                    Ligacoes.Remove($"{pai.Valor} -> {atual.Valor}");
+                    pai.Direita = null;
+                }
+            }
+
+            // Caso 2: Nó não possui filho na direita
+            else if(atual != null && atual.Direita == null){
+                Ligacoes.Remove($"{pai.Valor} -> {atual.Valor}");
+                Ligacoes.Remove($"{atual.Valor} -> {atual.Esquerda.Valor}");
+                if(atual == Raiz){
+                    Raiz = atual.Esquerda;
+                    Ligacoes.Add($"{pai.Valor} -> {atual.Esquerda.Valor}");
+                }else if(e_esquerda){
+                    pai.Esquerda = atual.Esquerda;
+                    Ligacoes.Add($"{pai.Valor} -> {atual.Esquerda.Valor}");
+                }else{
+                    pai.Direita = atual.Esquerda;
+                    Ligacoes.Add($"{pai.Valor} -> {atual.Esquerda.Valor}");
+                }
+            }
+
+            // Caso 3: Nó não possui filho na esquerda
+            else if(atual != null && atual.Esquerda == null){
+                Ligacoes.Remove($"{pai.Valor} -> {atual.Valor}");
+                Ligacoes.Remove($"{atual.Valor} -> {atual.Direita.Valor}");
+                if(atual == Raiz){
+                    Raiz = atual.Direita;
+                    Ligacoes.Add($"{pai.Valor} -> {atual.Direita.Valor}");
+                }else if(e_esquerda){
+                    pai.Esquerda = atual.Direita;
+                    Ligacoes.Add($"{pai.Valor} -> {atual.Direita.Valor}");
+                }else{
+                    pai.Direita = atual.Direita;
+                    Ligacoes.Add($"{pai.Valor} -> {atual.Direita.Valor}");
+                }
+            }
+
+            //Caso 4: Nó possui dois filhos
+            else{
+                No? sucessor = BuscarSucessor(atual);
+                Ligacoes.Remove($"{pai.Valor} -> {atual.Valor}");
+                Ligacoes.Remove($"{atual.Valor} -> {sucessor.Valor}");
+                Ligacoes.Remove($"{atual.Valor} -> {atual.Esquerda.Valor}");
+                Ligacoes.Remove($"{atual.Valor} -> {atual.Direita.Valor}");
+                if(atual == Raiz){
+                    Ligacoes.Add($"{Raiz.Valor} -> {sucessor.Valor}");
+                    Raiz = sucessor;
+                }else if(e_esquerda){
+                    Ligacoes.Add($"{pai.Valor} -> {sucessor.Valor}");
+                    pai.Esquerda = sucessor;
+                }else{
+                    Ligacoes.Add($"{pai.Valor} -> {sucessor.Valor}");
+                    pai.Direita = sucessor;
+                }
+
+                Ligacoes.Add($"{sucessor.Valor} -> {atual.Esquerda.Valor}");
+                //Ligacoes.Add($"{sucessor.Valor} -> {atual.Direita.Valor}");
+
+                sucessor.Esquerda = atual.Esquerda;
+            }
+
+        }
+
+        private No? BuscarSucessor(No? no){
+            No? paiSucessor = no;
+            No? sucessor = no;
+            No? atual = no.Direita;
+
+            while(atual != null){
+                paiSucessor = sucessor;
+                sucessor = atual;
+                atual = atual.Esquerda;
+            }
+
+            if(sucessor != no.Direita){
+                paiSucessor.Esquerda = sucessor.Direita;
+                sucessor.Direita = no.Direita;
+            }
+
+            return sucessor;
         }
 
         // Pré-ordem
